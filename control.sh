200b4780a90a43c99f89b3530a296732f03cb2db
Baryshnikov Nikolay <root@k0d.ru>
2018-08-01 12:05:16 +0300

    control script review

--- control.sh
+++ control.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 COMMAND=$1
-REGISTRY=''
-CONTAINER_NAME=''
+REGISTRY='0.0.0.0:5000'
+CONTAINER_NAME='python-basic'
 PORTS='8000:8000'
 DIRECTORY=$(pwd)
 
 kill_container () {
+    echo '=============KILL OLD================='
     if [ -n $(docker container ps -q -f name=${CONTAINER_NAME}) ]; then
         docker kill ${CONTAINER_NAME}
     fi
@@ -23,7 +24,7 @@ build (){
     docker build -t ${REGISTRY}/${CONTAINER_NAME} ${DIRECTORY}/.
 }
 
-d
+
 case ${COMMAND} in
 'push')
 build
@@ -32,24 +33,21 @@ echo '=============PUSH================='
 ;;
 
 'logs')
-build
 echo '=============LOGS================='
-kill_container
-    docker run -d -p ${PORTS} --name ${CONTAINER_NAME} ${REGISTRY}/${CONTAINER_NAME}
-    docker log ${CONTAINER_NAME} -f
+    docker logs -f ${CONTAINER_NAME}
 ;;
 
 'local')
 build
-echo '=============LOCAL================='
 kill_container
+echo '=============LOCAL================='
     docker run -it -p ${PORTS} --name ${CONTAINER_NAME} ${REGISTRY}/${CONTAINER_NAME} bash
 ;;
 
 'daemon')
 build
-echo '=============DAEMON================='
 kill_container
+echo '=============DAEMON================='
     docker run -d -p ${PORTS} --name ${CONTAINER_NAME} ${REGISTRY}/${CONTAINER_NAME}
     sleep 2
     docker ps
@@ -57,16 +55,16 @@ kill_container
 
 'start')
 build
-echo '=============START================='
 kill_container
+echo '=============START================='
     docker run -d -p ${PORTS} --name ${CONTAINER_NAME} ${REGISTRY}/${CONTAINER_NAME}
     sleep 2
     docker ps
 ;;
 
 'stop')
-echo '=============STOP================='
 kill_container
+echo '=============STOP================='
     sleep 2
     docker ps
 ;;
